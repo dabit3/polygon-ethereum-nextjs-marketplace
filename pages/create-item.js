366b2fde381dd0974e4f60609eb1f7ce6265ab56
nonlin-lin-chaos-order-etc-etal <19966907+nonlin-lin-chaos-order-etc-etal@users.noreply.github.com>
2022-01-27 04:18:27 +0800

    Update create-item.js

--- pages/create-item.js
+++ pages/create-item.js
@@ -3,6 +3,7 @@ import { ethers } from 'ethers'
 import { create as ipfsHttpClient } from 'ipfs-http-client'
 import { useRouter } from 'next/router'
 import Web3Modal from 'web3modal'
+import Image from 'next/image'
 
 const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
 
@@ -102,7 +103,14 @@ export default function CreateItem() {
         />
         {
           fileUrl && (
-            <img className="rounded mt-4" width="350" src={fileUrl} />
+            <>
+              <Image
+                src={fileUrl}
+                alt="A file as a picture"
+                width={350}
+                className="rounded mt-4"
+              />
+            </>
           )
         }
         <button onClick={createMarket} className="font-bold mt-4 bg-pink-500 text-white rounded p-4 shadow-lg">
