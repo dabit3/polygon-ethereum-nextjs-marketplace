1b14ec6d7d7007b2f78f5415c0f035304a05b81c
nonlin-lin-chaos-order-etc-etal <19966907+nonlin-lin-chaos-order-etc-etal@users.noreply.github.com>
2022-01-27 04:27:14 +0800

    <img> lint fix

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import { ethers } from 'ethers'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import Web3Modal from "web3modal"
+import Image from 'next/image'
 
 import {
   nftaddress, nftmarketaddress
@@ -68,7 +69,12 @@ export default function Home() {
           {
             nfts.map((nft, i) => (
               <div key={i} className="border shadow rounded-xl overflow-hidden">
-                <img src={nft.image} />
+                <>
+                  <Image
+                    src={nft.image}
+                    alt="NFT image"
+                  />
+                </>
                 <div className="p-4">
                   <p style={{ height: '64px' }} className="text-2xl font-semibold">{nft.name}</p>
                   <div style={{ height: '70px', overflow: 'hidden' }}>
